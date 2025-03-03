6bf21ebb749f801b2a16a3200158d9406b602161
Diksha Sharma <sharmadiksha0523@gmail.com>
2025-03-03 21:33:18 +0530

    update url

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = import.meta.env.VITE_BASE_URL;
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://startupthon-dp34.onrender.com/api/v1";
 
 export const authEndpoints = {
   login: BASE_URL + "/users/login",
